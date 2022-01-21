55ad85bf03088628cdd8ff866d60a9a055bf0163
taishi <taishi.castle@gmail.com>
2022-01-21 12:21:20 +0900

    circleci変更

--- learning_app/learning/settings.py
+++ learning_app/learning/settings.py
@@ -12,7 +12,6 @@ env.read_env(os.path.join(BASE_DIR, '.env.dev'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.get_value('DEBUG', cast=bool, default=False)
